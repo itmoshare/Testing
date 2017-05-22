f64e85288a8f7fb3d8d350e57d9d9a63eb30cee1
shumantt <shumantt@mail.ru>
2017-05-22 23:48:30 +0300

    lab3 android test widget adding

---
+++ lab3Android/dotnet/AppiumDotNetSample/Tests/AddWidgetTest.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AppiumDotNetSample.Tests
+{
+    
+    public class AddWidgetTest : AndroidBaseTest
+    {
+        [Test]
+        public void CheckAppRunning()
+        {
+
+            driver.PressKeyCode(AndroidKeyCode.Home);
+            Thread.Sleep(1000);
+
+            var clickableCountBefore = driver.FindElementsByAndroidUIAutomator("new UiSelector().clickable(true)").Count;
+            
+            var pressAndHold = new TouchAction(driver);
+
+            var action = pressAndHold.LongPress(500, 700);
+
+            //SUPER DIRTY HACK
+            var stepsField = pressAndHold.GetType()
+                            .GetField("steps", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            var field = (stepsField.GetValue(pressAndHold) as IEnumerable<object>).ElementAt(0);
+            var dic = field.GetType()
+                .GetField("parameters", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                .GetValue(field) as IDictionary;
+            dic["action"] = "longPress";
+
+            action.Perform();
+
+            var widgetsButton = driver.FindElementByAndroidUIAutomator("new UiSelector().textContains(\"WIDGETS\")");
+            widgetsButton.Click();
+
+
+            var kateSmall = driver.FindElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView("
+                        + "new UiSelector().textContains(\"Kate Mobile 1x1\"));");
+
+
+
+            var pressAndHoldKateWidget = new TouchAction(driver)
+                .Press(kateSmall.Location.X + 10, kateSmall.Location.Y + 20).Wait(500).MoveTo(100, -200)
+                .Wait(2000).Release();
+            pressAndHoldKateWidget.Perform();
+
+
+            var clickableCountAfter = driver.FindElementsByAndroidUIAutomator("new UiSelector().clickable(true)").Count;
+
+            Assert.True(clickableCountAfter > clickableCountBefore);
+        }
+    }
+}
---
+++ lab3Android/dotnet/AppiumDotNetSample/Tests/AndroidBaseTest.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AppiumDotNetSample.Tests
+{
+    public class AndroidBaseTest
+    {
+        public AndroidDriver<AppiumWebElement> driver;
+
+        [OneTimeSetUp]
+        public void BeforeAll()
+        {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+
+            capabilities.SetCapability("deviceName", "Android Emulator");
+            capabilities.SetCapability("platformName", "Android");
+            capabilities.SetCapability("app", $"{Environment.CurrentDirectory}/AppiumDotNetSample/Kate.apk");//full path needed((
+            capabilities.SetCapability("noReset", true);
+            capabilities.SetCapability("fullReset", false);
+            capabilities.SetCapability("appActivity", "com.perm.kate.MainActivity");
+            driver = new AndroidDriver<AppiumWebElement>(
+                                       new Uri("http://127.0.0.1:4723/wd/hub"),
+                                           capabilities);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        }
+
+        
+
+        [OneTimeTearDown]
+        public void AfterAll()
+        {
+            if (driver != null)
+            {
+                driver.Quit();
+            }
+        }
+    }
+}
